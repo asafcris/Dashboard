beffc1ba7a50b12bf7631f1309b68e82b85bfbe9
Alex Sandro <30936234+asafcris@users.noreply.github.com>
2018-07-11 11:27:11 -0300

    adicioando informações na tela inicial

---
+++ Dashboard.Domain/Model/Venda.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dashboard.Domain.Model
+{
+    public class Venda
+    {
+        public int Id { get; set; }
+        public string Produto { get; set; }
+        public Decimal Valor { get; set; }
+        public DateTime DataVenda { get; set; }
+
+        public IEnumerable<Venda> ListagemVendasMocada()
+        {
+            var venda1 = new Venda { Id = 1, DataVenda = DateTime.Now, Produto = "A", Valor = 20 };
+            var venda2 = new Venda { Id = 1, DataVenda = DateTime.Now, Produto = "B", Valor = 30 };
+            var venda3 = new Venda { Id = 1, DataVenda = DateTime.Now, Produto = "C", Valor = 45 };
+            var venda4 = new Venda { Id = 1, DataVenda = DateTime.Now.AddDays(-1), Produto = "D", Valor = 10 };
+            var venda5 = new Venda { Id = 1, DataVenda = DateTime.Now.AddDays(-1), Produto = "E", Valor = 3 };
+            var venda6 = new Venda { Id = 1, DataVenda = DateTime.Now.AddDays(-2), Produto = "F", Valor = 2 };
+            var venda7 = new Venda { Id = 1, DataVenda = DateTime.Now.AddDays(-2), Produto = "G", Valor = 25 };
+
+            var vendas = new List<Venda>();
+            vendas.Add(venda1);
+            vendas.Add(venda2);
+            vendas.Add(venda3);
+            vendas.Add(venda4);
+            vendas.Add(venda5);
+            vendas.Add(venda6);
+            vendas.Add(venda7);
+
+            return vendas;
+        }
+
+    }
+}
--- Dashboard/Controllers/HomeController.cs
+++ Dashboard/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using Dashboard.Domain.Model;
 using Microsoft.AspNetCore.Mvc;
 using Dashboard.Models;
 
@@ -12,26 +13,17 @@ namespace Dashboard.Controllers
     {
         public IActionResult Index()
         {
-            return View();
-        }
+           var venda = new Venda();
 
-        public IActionResult About()
-        {
-            ViewData["Message"] = "Your application description page.";
+            ViewBag.Vendas = venda.ListagemVendasMocada().ToList().GroupBy(x => DateTime.Parse(x.DataVenda.ToShortDateString()))
+                .OrderByDescending(x => x.Key)
+                .Select(x => new KeyValuePair<string, int>(x.Key.ToString("dd/MM/yyyy"), x.Count()))
+                .ToList();
 
-            return View();
-        }
 
-        public IActionResult Contact()
-        {
-            ViewData["Message"] = "Your contact page.";
+            ViewBag.ResumoMes = new KeyValuePair<decimal, int>(venda.ListagemVendasMocada().Sum(x => x.Valor), venda.ListagemVendasMocada().Count());
 
             return View();
-        }
-
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
 }
